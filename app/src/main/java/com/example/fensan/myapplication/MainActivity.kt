c8c94802092427237c7d1a25e9d476cb776e7fa3
sanfenr <sanfenruxi1@163.com>
2017-05-31 14:57:40 +0800

    创建项目

---
+++ app/src/main/java/com/example/fensan/myapplication/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.fensan.myapplication
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by fensan on 2017/5/27.
+ */
+class MainActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
